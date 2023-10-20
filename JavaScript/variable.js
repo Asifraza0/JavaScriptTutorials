f4b90b000f2b4f0f9f28018fc83ff6485e8067f1
ASIF RAZA <94372769+Asifraza0@users.noreply.github.com>
2023-10-20 19:10:15 +0000

    variables changes

---
+++ JavaScript/variable.js
@@ -0,0 +1,13 @@
+const accountID = 123456
+let accountname = 'constant'
+var accountpassword = '12345667'
+city = 'Meerut'
+
+console.table([accountID, accountname, accountpassword, city]);
+// console.log(accountID)
+
+// accountID = 7656734564376736
+accountname = 'hdfgffvfhag'
+accountpassword = '5647562389746576546574'
+city = 'sdhfkjsdfdsgfhsgfhfgfhdgfhg UP'
+console.table([accountID, accountname, accountpassword, city]);
