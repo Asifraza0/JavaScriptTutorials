9b54841ea494b7d84dd4c6c5a42f11fc1b248084
ASIF RAZA <94372769+Asifraza0@users.noreply.github.com>
2023-10-21 06:35:56 +0000

    third

---
+++ datatypes/datatypes.js
@@ -0,0 +1,33 @@
+// alert(3+3)  we are using nodejs note browser
+
+let name = 'khan'
+let age = 19
+let isLoggedIn = false
+let nulls = null
+let undefined = ({})
+
+
+//// preamt dataType
+
+// number => 2 to power 34
+// bigInt => 983748735678456645
+// string => 'sgfyfvs' "urydftyrfewyguf"
+// boolean => true/false
+// null => standloan value
+// undefined => 
+// symbol => unique
+
+// OBJECT IN DATATYPE
+console.log(typeof null)  // Object
+console.log(typeof undefined) // undefined
+
+
+
+let some = 33
+
+let stringNumber = String(some)
+console.log(stringNumber)
+console.log(typeof some)
+
+
+// string is not convertable but number always convert in string
